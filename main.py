255cc48c59d7955c9b9937fb9f7cf277a3b9c313
tebzaselepe <19845678@Tebza>
2023-01-21 06:01:44 +0200

    added login form

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 import streamlit as st
+import yaml
+from yaml.loader import SafeLoader
+import streamlit.components.v1 as components
+
+from hasher import Hasher
+from authenticate import Authenticate
 
 def main():
-    st.balloons()
+    # hashed_passwords = Hasher(['123', '123']).generate()
+    # st.write(hashed_passwords)
+    # Loading config file
+    with open('./config.yaml') as file:
+        config = yaml.load(file, Loader=SafeLoader)
 
+    # Creating the authenticator object
+    authenticator = Authenticate(
+        config['credentials'],
+        config['cookie']['name'], 
+        config['cookie']['key'], 
+        config['cookie']['expiry_days'],
+        config['preauthorized']
+    )
+    # creating a login widget
+    name, authentication_status, username = authenticator.login('Login', 'main')
+    if authentication_status:
+        authenticator.logout('Logout', 'main')
+        st.write(f'Welcome *{name}*')
+        st.title('Some content')
+    elif authentication_status == False:
+        st.error('Username/password is incorrect')
+    elif authentication_status == None:
+        st.warning('Please enter your username and password')
 if __name__ == "__main__":
     main()
